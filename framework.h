00a95c2d9f15bef5dcd2b5de3cf9ee39d5e1cef7
mochinekoo <mochinekoo0113@gmail.com>
2026-05-04 10:55:14 +0900

    [change] 変更しました

--- framework.h
+++ framework.h
@@ -12,6 +12,6 @@ namespace GameScreen {
 	inline const int WIDTH = 1280; //画面の幅
 	inline const int HEIGHT = 720; //画面の高さ
 	inline const int COLORBIT = 32; //カラービット
-	inline const int BACKGROUND[3] = { 0, 0, 0 }; //画面の背景
+	inline const int BACKGROUND[3] = { 255, 255, 255}; //画面の背景
 	inline const int FPS = 60; //画面のフレームレート
 }
